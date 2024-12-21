114fb0c7a35ece4ec7a566b2097485bcd96c3f1e
mlebiedzinski <lebiedzinski.m@northeastern.edu>
2024-12-21 17:17:02 -0500

    build

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
         <section id="about">
             <div>
                 <div className={styles.aboutTag}>
-                    <img src="../../../assets/about/aboutTag.png" alt="aboutTag" />
+                    <img src="../../../../assets/about/aboutTag.png" alt="aboutTag" />
                 </div>
                 <img className={styles.me} src="../../../../assets/about/me.png" alt="me" />
                 <div className={styles.aboutText}>From an early age, I had a natural knack for <span>creativity</span> and an eye for design. I loved experimenting with <span>visual storytelling</span>, color, and layout, and developed a strong appreciation well-crafted design. As I got older, my curiosity expanded to <span>technology</span>, and I became interested in how <span>software</span> could be a <span>creative medium</span>. When I came to college, I decided to add <span>software engineering</span> to my toolkit, diving into the technical side to gain skills in development and broaden my capacity to create <span>interactive, impactful user experiences</span>.
