d07e1b8c92f7d7587d22db151f89c14fe1335b2a
mlebiedzinski <lebiedzinski.m@northeastern.edu>
2025-01-19 21:20:27 -0500

    pauls proto link

--- src/components/Projects/CaseStudies/Pauls.jsx
+++ src/components/Projects/CaseStudies/Pauls.jsx
@@ -13,12 +13,19 @@ export const Pauls = () => {
 
   return (
     <div>
-      <h1>Paul's</h1>
+      {/* <h1>Paul's</h1>
       <h2>I am building a website to represent a cafe called Paul's! 
         I am using this project as a chance to try out new web development trends and skills. Stay tuned!</h2>
-        <br/>
-        {/* <h6>Sneak Peek</h6>
-        <img src="paulsFigma.png" alt="Description"></img> */}
+        <br/> */}
+        <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+        <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" 
+        width="800" 
+        height="450" 
+        src="https://embed.figma.com/proto/15NR6BxSl0ovtcXYZJ41bb/Untitled?page-id=0%3A1&node-id=5-36&viewport=-549%2C140%2C0.19&scaling=scale-down&content-scaling=fixed&embed-host=share" 
+        allowfullscreen>
+            
+        </iframe>
+        </div>
    </div>
   );
 };
